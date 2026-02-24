f8a939c9e7c9e56b92d25c1525f5d487afa0e812
lyedsonmucale0-commits <lyedsonmucale0@gmail.com>
2026-02-24 14:33:25 +0200

    Create HolyPlay.js

---
+++ apps/HolyPlay.js
@@ -0,0 +1,21 @@
+window.NosPlayApps = window.NosPlayApps || [];
+
+window.NosPlayApps.push({
+  nome: "HolyPlay DEMO",
+  categoria: "Educação",
+  versao: "3.1.0",
+  tamanho: "32MB",
+  desc: "HolyPlay DEMO é um aplicativo cristão projetado para oferecer uma experiência completa de entretenimento e aprendizado.",
+  icon: "https://i.imgur.com/4Z9UNwL.png",
+  shots: [
+    "https://i.imgur.com/vKuE823.jpeg",
+    "https://i.imgur.com/18bFIN2.jpeg",
+    "https://i.imgur.com/qyBzUaE.jpeg",
+    "https://i.imgur.com/FVzr421.jpeg",
+    "https://i.imgur.com/xh4gX3h.jpeg",
+    "https://i.imgur.com/Aq8zr3O.jpeg",
+    "https://i.imgur.com/vD5udWA.jpeg",
+    "https://i.imgur.com/Zi5kAjp.jpeg"
+  ],
+  tag: "V1.2"
+});
