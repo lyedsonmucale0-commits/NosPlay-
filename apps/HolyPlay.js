e71bd2b8f02c4c5651937ace0f325153a70b8a6a
lyedsonmucale0-commits <lyedsonmucale0@gmail.com>
2026-02-28 00:32:52 +0200

    Update HolyPlay.js

--- apps/HolyPlay.js
+++ apps/HolyPlay.js
@@ -4,9 +4,10 @@ window.NosPlayApps.push({
   nome: "HolyPlay DEMO",
   categoria: "Educação",
   versao: "3.1.0",
-  tamanho: "32MB",
+  tamanho: "15MB",
   desc: "HolyPlay DEMO é um aplicativo cristão projetado para oferecer uma experiência completa de entretenimento e aprendizado.",
   icon: "https://i.imgur.com/4Z9UNwL.png",
+  apk: "https://github.com/lyedsonmucale0-commits/NosPlayAPK/releases/download/V2.0/HolyPlay.apk",
   shots: [
     "https://i.imgur.com/vKuE823.jpeg",
     "https://i.imgur.com/18bFIN2.jpeg",
@@ -17,5 +18,5 @@ window.NosPlayApps.push({
     "https://i.imgur.com/vD5udWA.jpeg",
     "https://i.imgur.com/Zi5kAjp.jpeg"
   ],
-  tag: "V1.2"
+  tag: "V2.0"
 });
